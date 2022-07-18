b5eefd67a7a8a6d2af1921eed37294c2624aff78
leom97 <leo--msn@hotmail.com>
2022-07-19 00:16:43 +0200

    Generalize transfer matrices to arbitrary boundaries

--- code/utilities/overloads.py
+++ code/utilities/overloads.py
@@ -3,6 +3,9 @@ from dolfin_adjoint import *
 import numpy as np
 from pyadjoint import Block
 from pyadjoint.overloaded_function import overload_function
+from tqdm import tqdm
+import logging
+
 
 ########################################################################################################################
 # Note, we heavily rely on the usage of first order finite elements here...
@@ -13,29 +16,33 @@ from pyadjoint.overloaded_function import overload_function
 # 2) building FEM base: https://fenicsproject.discourse.group/t/accessing-the-basis-function-of-a-finite-element/2426/2
 
 
-def compute_spherical_transfer_matrix(V_vol, V_sph):
+def compute_spherical_transfer_matrix(V_vol, V_sph, p=None):
     """
     We compute a matrix that from a function defined on V_sph (a spherical function), actually, from the vector of its
     nodal values, returns a vector, the nodal values of a function on a volumetric function space V_vol. The resulting
-    function if f(x) = g(x/|x|), for g the input function on the sphere. Note, the volumetric domain must not contain 0!
+    function if f(x) = g(x/|x|), for g the input function on the sphere. Note, the volumetric domain must not contain p!
     :param V_vol: volumetric function space
     :param V_sph: spherical function space
+    :param p: the point with respect to which the domain is star shaped. Must be a (n,1) array
     :return: M, the transfer matrix
     """
+
+    if p is None:
+        p = np.zeros(V_vol.mesh().topology().dim())[None, :]
 
     mesh_vol = V_vol.mesh()
     mesh_sph = V_sph.mesh()
 
     # Coordinates but on the unit sphere, in dof order
 
-    vol_points = mesh_vol.coordinates()[:]
-    vol_points_sphere = vol_points / (np.linalg.norm(vol_points, axis=1)[:, None])  # note, in vertex order
+    vol_points = mesh_vol.coordinates()[:] - p
+    vol_points_sphere = vol_points / (np.linalg.norm(vol_points, axis=-1)[:, None])  # note, in vertex order
     vol_points_sphere_dof = vol_points_sphere[dof_to_vertex_map(V_vol),
                             :]  # this means that the i-th nodal value of a function on V_vol, is the function value at vol_points_sphere_dof[i]
 
     # Basis functions on the sphere
 
-    tree_sph = mesh_sph.bounding_box_tree()  # todo: this will be an attribute of a class later on
+    tree_sph = mesh_sph.bounding_box_tree()
 
     def basis_sph(j, x):
         # here, i is dof order
@@ -64,44 +71,154 @@ def compute_spherical_transfer_matrix(V_vol, V_sph):
 
     M = np.zeros((vol_dim, sph_dim))  # operator from sphere to volume
 
-    for j in sphere_dofs:
+    logging.info(f"Assembling transfer matrix from spherical functions into scalar functions")
+    for j in tqdm(sphere_dofs):
         for i in volume_dofs:
             M[i, j] = basis_sph(j, vol_points_sphere_dof[i, :])
 
     return M
 
-def backend_transfer_from_sphere(g, M, V_vol):
+
+# def backend_transfer_from_sphere(g, M, V_vol):
+#     """
+#     Takes g, a function on the sphere, returns f, f(x)=g(x/|x|)
+#     :param g: function on spherical mesh
+#     :param M: transfer matrix from that spherical mesh to V_vol
+#     :param V_vol: volumetric mesh
+#     :return: f
+#     """
+#     gv = g.vector()[:]
+#     f = Function(V_vol)
+#     f.vector()[:] = M @ gv
+#     return f
+
+def radial_function_to_square(x, L=1):
     """
-    Takes g, a function on the sphere, returns f, f(x)=g(x/|x|)
-    :param g: function on spherical mesh
-    :param M: transfer matrix from that spherical mesh to V_vol
-    :param V_vol: volumetric mesh
-    :return: f
+    Computes the radial function describing a square with center 0 and side L
+    :param x: Nx2 array of coordinates different from 0
+    :param L:
+    :return:
     """
+
+    x_hat = x / (np.linalg.norm(x, axis=-1)[:, None])
+
+    mask_ud = (-np.sqrt(2) / 2 <= x_hat[:, 0]) * (x_hat[:, 0] <= np.sqrt(2) / 2)
+    mask_lr = (1 - mask_ud).astype(bool)
+
+    output = np.zeros(x_hat.shape[0])
+
+    x_ud = x_hat[mask_ud, :]
+    x_lr = x_hat[mask_lr, :]
+
+    output[mask_lr] = 0.5 * L / np.sqrt(1 - x_lr[:, 1] ** 2)
+    output[mask_ud] = 0.5 * L / np.sqrt(1 - x_ud[:, 0] ** 2)
+
+    return output
+
+
+def compute_radial_displacement_matrix(M, VD, f_D=None, p=None, eps=1):
+    """
+    :param f_D: radial lambda function describing the external boundary of our pseudo-annulus. It is assumed infinite dimensional for now (todo: relax this)
+    :param eps, p: out reference pseudo-annulus has a hole of radius eps centered at p
+    :param M:
+    :param VD:
+    :param p: a point of shape (1,n)
+    :return: M2, a matrix taking the nodal values of a function defined on the sphere, to its radial displacement vector field (nodal coordinates)
+    """
+
+    # dof to vertex map, to index the first component of mesh_coords
+    d2v = dof_to_vertex_map(VD)[0::2] // 2
+
+    mesh_coords = VD.mesh().coordinates()[:] - p  # note the translation!!
+    mesh_coords_dof = mesh_coords[d2v, :]
+    mesh_coords_dof_norms = np.linalg.norm(mesh_coords_dof, axis=-1)[:, None]
+
+    border_coords = f_D(mesh_coords_dof)
+
+    multiplier = (border_coords[:, None] - mesh_coords_dof_norms) / (
+            border_coords[:, None] - eps) * mesh_coords_dof / mesh_coords_dof_norms
+
+    multiplier[np.abs(border_coords - np.squeeze(mesh_coords_dof_norms)) <= DOLFIN_EPS, :] = 0
+
+    M2 = np.zeros((VD.dim(), M.shape[1]))
+
+    # Let's do it inefficiently at first
+    logging.info(f"Assembling transfer matrix from spherical functions into vector fields")
+    for i in tqdm(range(M.shape[0])):
+        M_row_i = M[i, :][None, :]
+        multiplier_i = multiplier[i, :][:, None]
+
+        two_rows_i = multiplier_i @ M_row_i
+
+        M2[(2 * i):(2 * i + 2), :] = two_rows_i
+
+    return M2
+
+
+def backend_radial_displacement(g, M2, VD):
+    """
+    :param g: a spherical function on S^n
+    :param M2: transfer matrix S^n -> \Omega, for \Omega star shaped wrt p
+    :param VD: CG1 vector function space on \Omega
+    :return: W, W(x) = (f_D-|x-p|)*(x-p)/|x-p| * g((x-p)/|x-p|), if the original domain is p-star shaped
+    """
+
     gv = g.vector()[:]
-    f = Function(V_vol)
-    f.vector()[:] = M @ gv
-    return f
+    W = Function(VD)
+    W.vector()[:] = M2 @ gv
+    return W
 
 
 # dolfin-adjoint version
-class TranferFromSphereBlock(Block):
-    def __init__(self, g, M, V_vol, **kwargs):
-        super(TranferFromSphereBlock, self).__init__()
+# class TranferFromSphereBlock(Block):
+#     def __init__(self, g, M, V_vol, **kwargs):
+#         super(TranferFromSphereBlock, self).__init__()
+#         self.kwargs = kwargs
+#         self.add_dependency(g)
+#
+#         self.M = M  # 'interpolation' matrix bringing us from g.function_space() to V_vol
+#         self.V_vol = V_vol  # the volumetric function space, onto which we extend g radially
+#         self.V_sph_dim = g.function_space().dim()
+#
+#     def __str__(self):
+#         return 'TranferFromSphereBlock'
+#
+#     def recompute_component(self, inputs, block_variable, idx, prepared):
+#         # note, inputs is the list containing all the dependencies we specified above
+#         # therefore it will be a list of length 1, containing exactly func (actually, a copy of it)
+#         return backend_transfer_from_sphere(inputs[0], self.M, self.V_vol)
+#
+#     def evaluate_adj_component(self, inputs, adj_inputs, block_variable, idx, prepared=None):
+#         # Note: everything in automatic differentiation is just a vector. So, this method takes vectors (the adj_inputs) and returns vectors
+#
+#         # Explanation
+#         # A function is e.g. z = f(x,y). If idx==1, here, we must return (d/dy f (x,y))^T * z
+#         # In the specific case, z = f(x), so idx == 0 always
+#
+#         adj_input = adj_inputs[0]  # this is a vector, with which we test the adjoint jacobian
+#         adj_action = self.M.T @ adj_input
+#         output = Vector(MPI.comm_world, self.V_sph_dim)
+#         output[:] = adj_action
+#
+#         return output
+
+class RadialDisplacementBlock(Block):
+    def __init__(self, g, M2, VD, **kwargs):
+        super(RadialDisplacementBlock, self).__init__()
         self.kwargs = kwargs
         self.add_dependency(g)
 
-        self.M = M  # 'interpolation' matrix bringing us from g.function_space() to V_vol
-        self.V_vol = V_vol  # the volumetric function space, onto which we extend g radially
+        self.M2 = M2  # matrix bringing us from g.function_space() to VD
+        self.VD = VD  # the volumetric function space, vector fields version
         self.V_sph_dim = g.function_space().dim()
 
     def __str__(self):
-        return 'TranferFromSphereBlock'
+        return 'RadialDisplacementBlock'
 
     def recompute_component(self, inputs, block_variable, idx, prepared):
         # note, inputs is the list containing all the dependencies we specified above
         # therefore it will be a list of length 1, containing exactly func (actually, a copy of it)
-        return backend_transfer_from_sphere(inputs[0], self.M, self.V_vol)
+        return backend_radial_displacement(inputs[0], self.M2, self.VD)
 
     def evaluate_adj_component(self, inputs, adj_inputs, block_variable, idx, prepared=None):
         # Note: everything in automatic differentiation is just a vector. So, this method takes vectors (the adj_inputs) and returns vectors
@@ -111,11 +228,12 @@ class TranferFromSphereBlock(Block):
         # In the specific case, z = f(x), so idx == 0 always
 
         adj_input = adj_inputs[0]  # this is a vector, with which we test the adjoint jacobian
-        adj_action = self.M.T @ adj_input
-        output = Vector(Vector(MPI.comm_world, self.V_sph_dim))
+        adj_action = self.M2.T @ adj_input
+        output = Vector(MPI.comm_world, self.V_sph_dim)
         output[:] = adj_action
 
         return output
 
 
-transfer_from_sphere = overload_function(backend_transfer_from_sphere, TranferFromSphereBlock)
+# transfer_from_sphere = overload_function(backend_transfer_from_sphere, TranferFromSphereBlock)
+radial_displacement = overload_function(backend_radial_displacement, RadialDisplacementBlock)
