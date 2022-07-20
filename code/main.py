f7f172e27696afd124eb9ea3ad7737e5c1c13bef
leom97 <leo--msn@hotmail.com>
2022-07-20 13:35:16 +0200

    Slight reformatting of main file

--- code/main.py
+++ code/main.py
@@ -3,11 +3,12 @@ from dolfin_adjoint import *
 import matplotlib.pyplot as plt
 import logging
 import numpy as np
+import pickle
 
 from utilities.meshing import AnnulusMesh, CircleMesh
 from utilities.pdes import HeatEquation, TimeExpressionFromList
 from utilities.overloads import compute_spherical_transfer_matrix, \
-    compute_radial_displacement_matrix, radial_displacement
+    compute_radial_displacement_matrix, radial_displacement, radial_function_to_square
 
 # %% Setting log and global parameters
 
@@ -27,11 +28,10 @@ set_log_level(LogLevel.ERROR)
 
 # %% Let's create a mesh and deform it... todo: move to separate file
 with stop_annotating():
-
     ode_scheme = "crank_nicolson"
 
-    p = np.array([0,0]) # star shape point
-    f_D = lambda x: 2*np.ones(x.shape[0])   # external point
+    p = np.array([0, 0])  # star shape point
+    f_D = lambda x: 2 * np.ones(x.shape[0])  # external point
 
     annulus = AnnulusMesh(resolution=.05)  # we can be very fine here
     circle = CircleMesh(resolution=.15)
@@ -44,10 +44,12 @@ with stop_annotating():
 
     q_ex = Function(V_sph)
     circle_coords = circle.mesh.coordinates()[dof_to_vertex_map(V_sph), :]
-    q_ex.vector()[:] = -.5 * circle_coords[:, 0] ** 2 + .7 * circle_coords[:, 1] ** 2
+    # q_ex.vector()[:] = -.5 * circle_coords[:, 0] ** 2 + .7 * circle_coords[:, 1] ** 2
+    r_sq = radial_function_to_square(circle_coords, L=1)
+    q_ex.vector()[:] = r_sq - 1
 
-    M = compute_spherical_transfer_matrix(V_vol, V_sph, p = p)
-    M2 = compute_radial_displacement_matrix(M, VD, p = p, f_D = f_D)
+    M = compute_spherical_transfer_matrix(V_vol, V_sph, p=p)
+    M2 = compute_radial_displacement_matrix(M, VD, p=p, f_D=f_D)
     W = radial_displacement(q_ex, M2, VD)
 
     ALE.move(annulus.mesh, W)
@@ -60,7 +62,8 @@ with stop_annotating():
 
     u0 = Function(V_vol)  # zero initial condition
     u_D_inner = Function(V_vol)  # zero inner BC
-    u_N = Expression('exp(-a/pow(t,2))*sin(3*t)*pow(x[0],2)', t=0, a=.05, degree=4)
+    # u_N = Expression('exp(-a/pow(t,2))*sin(3*t)*pow(x[0],2)', t=0, a=.05, degree=4)
+    u_N = Expression('exp(-a/pow(t,2))*sin(3*t)*(pow(x[0],2)-cos(4*x[1]))', t=0, a=.05, degree=5)
     marker_dirichlet = [3]  # 0 BC on inner ring
     marker_neumann = [2]
     T = 1.0
@@ -134,7 +137,7 @@ solution_list_w, _ = heqw.solve()
 J = 0
 logging.warning("Integral discretization only valid for implicit euler")
 for v, w, dt, t in zip(solution_list_v[1:], solution_list_w[1:], heqv.dts, heqv.times[1:]):
-    J+=assemble(dt*(v-w)**2*dx(annulus.mesh))*exp(-0.05/(T-t)**2)
+    J += assemble(dt * (v - w) ** 2 * dx(annulus.mesh))  # *exp(-0.05/(T-t)**2)
 
 j = ReducedFunctional(J, Control(q))
 
@@ -144,41 +147,53 @@ taylor_test(j, q, h)
 
 # %% The optimization
 
-q_opt = minimize(j, tol=1e-6, options={"ftol": 1e-14, "gtol": 1e-14, "maxiter": 800, "disp":True})
+q_opt, res = minimize(j, tol=1e-6, options={"ftol": 1e-14, "gtol": 1e-14, "maxiter": 1, "disp": True})
 plot(annulus.mesh)
 plt.show()
 
 taylor_test(j, q_opt, h)
 
+# %% Save results
+pickles_path = "/home/leonardo_mutti/PycharmProjects/masters_thesis/code/results/"
+pickle_name = "scipy_optimization_results"
+with open(pickles_path + pickle_name + '.pickle', 'wb') as handle:
+    pickle.dump(res, handle)
+
+# %% Take optimization results
+
+with open(pickles_path + pickle_name + '.pickle', 'rb') as handle:
+    pk = pickle.load(handle)
+
+# %%
+
+file = XDMFFile(pickles_path)
+infile = XDMFFile(pickles_path + "/result_file.xdmf")
+
 # %% Notes
 
 # With final smoothing (around 40 minutes with another simulation running in parallel)
-# Tit   = total number of iterations
-# Tnf   = total number of function evaluations
-# Tnint = total number of segments explored during Cauchy searches
-# Skip  = number of BFGS updates skipped
-# Nact  = number of active bounds at final generalized Cauchy point
-# Projg = norm of the final projected gradient
-# F     = final function value
-#            * * *
 #    N    Tit     Tnf  Tnint  Skip  Nact     Projg        F
 #    63    800    923      1     0     0   4.681D-08   5.325D-10
 #   F =   5.3247090595756105E-010
 # STOP: TOTAL NO. of ITERATIONS REACHED LIMIT
 
 # Without final smoothing (also 40 minutes): the cost is higher because there is no final smoothing here)
-# Tit   = total number of iterations
-# Tnf   = total number of function evaluations
-# Tnint = total number of segments explored during Cauchy searches
-# Skip  = number of BFGS updates skipped
-# Nact  = number of active bounds at final generalized Cauchy point
-# Projg = norm of the final projected gradient
-# F     = final function value
-#            * * *
 #    N    Tit     Tnf  Tnint  Skip  Nact     Projg        F
 #    63    800    971      2     0     0   5.054D-08   2.949D-09
 #   F =   2.9487536940310408E-009
 # STOP: TOTAL NO. of ITERATIONS REACHED LIMIT
+
+# With more complicated BCs, no final smoothing (in space)
+#    N    Tit     Tnf  Tnint  Skip  Nact     Projg        F
+#    63    800    923      1     0     0   3.278D-08   1.128D-09
+#   F =   1.1281012793695380E-009
+# STOP: TOTAL NO. of ITERATIONS REACHED LIMIT
+
+# With standard BCs, no final smoothing, turning a circle into a square (Note, it stopped at 172!)
+#    N    Tit     Tnf  Tnint  Skip  Nact     Projg        F
+#    63    172    184      1     0     0   3.419D-09   2.943D-11
+#   F =   2.9434237016963961E-011
+
 
 # %% Todo
 # 1) implement smoothing (bound on derivatives, could be interfaced with minimize)
@@ -189,3 +204,6 @@ taylor_test(j, q_opt, h)
 # 6) how important is e^-1/t^2 in the initial condition? Does it give use OOC of 2?
 # 7) implement automatic saving of results and simulation parameters
 # 8) solving the reference PDE on a much finer scale, does it help?
+# 9) complicate the boundary conditions to highly spatially varying
+# 10) add timing and profiling
+# 11) a smaller domain helps out?
