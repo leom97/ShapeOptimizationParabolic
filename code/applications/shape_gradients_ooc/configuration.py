004b72213515e77a7cb1922d4950a6756bccbc65
leom97 <leo--msn@hotmail.com>
2022-11-29 21:21:59 +0100

    Better configuration.py documentation.

--- code/applications/shape_gradients_ooc/configuration.py
+++ code/applications/shape_gradients_ooc/configuration.py
@@ -2,6 +2,9 @@ import numpy as np
 from dolfin import *
 
 """
+
+Configuration file for shape_gradient_ooc_verification.py
+
 Set:
 pde_dict["ode_scheme"] = "crank_nicolson", manual = False
 pde_dict["ode_scheme"] = "crank_nicolson", manual = True
@@ -14,6 +17,14 @@ experiment, see also section 4.2.2.
 
 """
 
+experiment_dict = {
+    "N_it": 7,  # how many refinements for the spatial mesh?
+    "dt_multiplier": 5,  # to relate dt and h, see section 4.2.2: dt = dt_multiplier * h^(1 or 2)
+}
+
+# This dictionary describes the domain on which the shape gradient is computed (domain), and on which the spherical
+# functions live (sphere)
+# We only used a 2D annulus for this
 geometry_dict = {
     "domain": {"type": "annulus", "resolution": None, "ext_refinement": 1.0, "int_refinement": 1.0, "inner_radius": 1,
                "outer_radius": 2,
@@ -21,28 +32,30 @@ geometry_dict = {
     "sphere": {"dimension": 2, "resolution": 0.5},
     "q_ex_lambda": 'lambda x: 0'
 }
+# The resolution of the domain mesh is dictated by the following vector: every entry correspond to a mesh width,
+# which should get progressively finer
+h_tentative = 1 / (2 ** np.arange(0, experiment_dict["N_it"]))
+h_tentative = h_tentative[experiment_dict["start_index"]:]
 
+# Change "ode_scheme" to "implicit_euler" or "crank_nicolson"
 pde_dict = {
     "ode_scheme": "crank_nicolson",
-    "u_N": "Expression('sin(3*t)*(pow(x[0],2)-cos(4*x[1]))', t=0, a=.05, p = np.pi, degree=5)",
-    "marker_dirichlet": [3],
-    "marker_neumann": [2],
-    "T": 1,
+    "marker_dirichlet": [3],  # interior of annulus
+    "marker_neumann": [2],  # exterior of annulus
+    "T": 1,  # final time of simulation
     "N_steps": None
 }
 
+# Describes the function \eta of figure 4.2, the smoothing parameter a = 0.005 could be tweaked
 cost_functional_dict = {
     "final_smoothing_lambda": "lambda t:  exp(-0.005/pow(t,2)) if t > DOLFIN_EPS else 0.0",
     "discretization": None,
     "H1_smoothing": 0
 }
 
-experiment_dict = {
-    "N_it": 7,  # how many refinements for the spatial mesh?
-    "dt_multiplier": 5, # to relate dt and h, see section 4.2.2
-}
-
-# smooth_displacements_dict["x"][i], smooth_displacements_dict["y"][i], represents the a smooth vector field
+# Smooth_displacements_dict["x"][i], smooth_displacements_dict["y"][i], represents a smooth vector field
+# Add as many smooth vector fields as you wish, respecting the syntax rules of
+# code/utilities/OOC_verification.py -> string_to_vector_field
 smooth_displacements_dict = {
     "x": ["x^3", ".5*x", ".5*y^3", ".2*y", ".2*x+.2*y^2", ".2*x+.2*y^2", ".2*x*y+.2*y*2", "-.5*x-.2*x*y", ".5*x+.4*x*y",
           "x"],
@@ -50,8 +63,9 @@ smooth_displacements_dict = {
           "-.3*y^3-.1*x*y", "y"]
 }
 
-# Pde data
-class _f(UserExpression):
+
+# Pde data: used to set up the states v, w of section 3.1
+class _f(UserExpression):  # Dirichlet datum
     def __init__(self, t, **kwargs):
         super().__init__(self, **kwargs)
         self.t = t
@@ -63,7 +77,7 @@ class _f(UserExpression):
         return ()
 
 
-class _g(UserExpression):
+class _g(UserExpression):  # Nuemann datum
     def __init__(self, t, **kwargs):
         super().__init__(self, **kwargs)
         self.t = t
@@ -74,4 +88,12 @@ class _g(UserExpression):
     def value_shape(self):
         return ()
 
-manual = True  # it means, manual expression of the shape gradient
+
+# Set:
+# pde_dict["ode_scheme"] = "crank_nicolson", manual = False
+# pde_dict["ode_scheme"] = "crank_nicolson", manual = True
+# to reproduce table 4.1, and:
+# pde_dict["ode_scheme"] = "implicit_euler", manual = False
+# for table 4.2.
+manual = True  # it means that the expression of the shape gradient is computed by us (True)
+# or by dolfin-adjoint (False)
