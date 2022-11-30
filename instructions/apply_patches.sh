e3824d46c3053e56cf259fb55009a895043352bd
leom97 <leo--msn@hotmail.com>
2022-11-30 01:58:14 +0100

    Update instructions

--- instructions/apply_patches.sh
+++ instructions/apply_patches.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Insert path to environment site-packages (e.g. \$HOME/anaconda3/envs/thesis_env/venv_39/lib/python3.9/site-packages/):"
+echo "Insert path to environment site-packages (e.g. \$HOME/anaconda3/envs/thesis_env/venv_39/lib/python3.9/site-packages, without final slash):"
 read site_packages_path
 
 cp "fixes/fenics/tools.py" $site_packages_path"/ffc/uflacs/tools.py"
